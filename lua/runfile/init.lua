74e554ac55cc73240109e0aee49d936208915960
Mikhail Yudin <fagci.nsk@gmail.com>
2022-06-02 02:22:30 +0700

    feat: setup support

--- lua/runfile/init.lua
+++ lua/runfile/init.lua
@@ -1,31 +1,54 @@
 local M = {}
 local utils = require('runfile.utils')
 
+M.default_mappings = {
+    ['applescript'] = 'osascript {f} {a}',
+    ['elixir'] = 'elixir {f} {a}',
+    ['erlang'] = 'escript {f} {a}',
+    ['ghci'] = 'ghci {f} {a}',
+    ['go'] = 'go run {f} {a}',
+    ['idris'] = 'idris {f} {a}',
+    ['java'] = 'javac -g:none {f} && java {r}',
+    ['javascript'] = 'node {f} {a}',
+    ['lua'] = 'lua {f} {a}',
+    ['moon'] = 'moon {f} {a}',
+    ['perl'] = 'perl {f} {a}',
+    ['php'] = 'php {f} {a}',
+    ['python'] = 'python {f} {a}',
+    ['ruby'] = 'ruby {f} {a}',
+    ['sh'] = '{f} {a}',
+}
+
+M.mappings = {}
+M._is_setup_runned = false
+
+-- Setup some options, for example: { mappings = { ['sh'] = 'zsh {f} {a}' } }
+M.setup = function(options)
+    M._is_setup_runned = true
+    M.mappings = vim.tbl_deep_extend(
+        'force',
+        M.default_mappings,
+        options.mappings or {}
+    )
+end
+
 -- Run opened file in shell with args
 M.run = function(...)
-    local mappings = {
-        ['applescript'] = 'osascript {f} {a}',
-        ['elixir'] = 'elixir {f} {a}',
-        ['erlang'] = 'escript {f} {a}',
-        ['ghci'] = 'ghci {f} {a}',
-        ['go'] = 'go run {f} {a}',
-        ['idris'] = 'idris {f} {a}',
-        ['java'] = 'javac -g:none {f} && java {r}',
-        ['javascript'] = 'node {f} {a}',
-        ['lua'] = 'lua {f} {a}',
-        ['moon'] = 'moon {f} {a}',
-        ['perl'] = 'perl {f} {a}',
-        ['php'] = 'php {f} {a}',
-        ['python'] = 'python {f} {a}',
-        ['ruby'] = 'ruby {f} {a}',
-        ['sh'] = '{f} {a}',
-    }
-
     local ft = vim.bo.filetype
 
-    local cmd = mappings[ft]
+    if not M._is_setup_runned then
+        print('[runfile.nvim] Setup not called, using default options')
+        M.setup({})
+    end
+
+    if ft == '' then
+        print('[runfile.nvim] No filetype detected by nvim')
+        return
+    end
+
+    local cmd = M.mappings[ft]
     if not cmd then
-        print('No support for ' .. ft)
+        print('[runfile.nvim] No support for ' .. ft)
         return
     end
 
